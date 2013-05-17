990753b32404aebb7de2388d35977153365b4c42
Cristovao Cordeiro <cris@pb-d-128-141-72-72.cern.ch>
2013-05-17 15:14:21 +0200

    Adding functional buildrelease.sh

--- etc/buildrelease.sh
+++ etc/buildrelease.sh
@@ -122,8 +122,8 @@ else
         sudo mount -t cifs //cerndfs.cern.ch/dfs $MOUNT_DIR -o user=$USER
 fi
 
-cp -fr $GIT_DIR/rpm/repodata/ $MOUNT_DIR/Websites/c/cern-cloudinit-modules/
-cp -f $GIT_DIR/rpm/cern*$NEW_REL.noarch.rpm $MOUNT_DIR/Websites/c/cern-cloudinit-modules/
+cp -fr $GIT_DIR/rpm/repodata/ $MOUNT_DIR'Websites/c/cern-cloudinit-modules/'
+cp -f $GIT_DIR/rpm/cern*$NEW_REL.noarch.rpm $MOUNT_DIR'Websites/c/cern-cloudinit-modules/'
 
 echo "Unmounting DFS..."
 sudo umount $MOUNT_DIR
